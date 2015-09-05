1a381d68a6fd46102e84f28140653fdf1c27c889
neil highley <neil@neilhighley.com>
2015-09-05 17:25:59 +0100

    added nav

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,39 @@ var router = express.Router();
 var helpers=[{id:1,
   name:"Neeil",
   description:"local hero",
-  location:"dog-cat-fish"},
+  location:"dog.cat.fish",
+  skills:["boat","strength","swimming"]},
   {id:2,
     name:"Carlos",
     description:"local superhero",
-    location:"shoe-feet-cheese"},
+    location:"shoe.feet.cheese",
+    skills:["swordfighting","farming","firstaid"]
+   },
   {id:3,
     name:"Martin",
     description:"elusive protector",
-    location:"shield-sword-sparrow"}
+    location:"shield.sword.sparrow",
+    skills:["welding","carpentry","strength","driver"]}
     ];
 
 var tasks_available=[
   { id:1,
     name:"I need a boat to get to the relief center",
     skills_required:["Boat","swimmer"],
-    location:"free-fish-friday",
+    location:"free.fish.friday",
     accepted:false,
     accepted_by:[]},
   { id:2,
     name:"We need towels at the relief center",
     skills_required:["Boat","swimmer"],
-    location:"good-green-girl",
+    location:"good.green.girl",
     accepted:true,
     accepted_by:"Neil"},
   { id:3,
     name:"Doctor required for triage at the relief center",
     skills_required:["Boat","swimmer"],
     accepted:false,
-  location:"good-green-girl"},
+  location:"good.green.girl"},
 ]
 /* GET home page. */
 router.get('/', function(req, res, next) {
